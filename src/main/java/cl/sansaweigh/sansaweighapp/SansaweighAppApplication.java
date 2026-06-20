b491205e8e2c6d32d8eafcdd8bbe614c9c2edf69
alvarojimenezg <alvarojg2002@hotmail.com>
2026-06-20 19:09:47 -0400

    Inicio

---
+++ src/main/java/cl/sansaweigh/sansaweighapp/SansaweighAppApplication.java
@@ -0,0 +1,13 @@
+package cl.sansaweigh.sansaweighapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SansaweighAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SansaweighAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cl/sansaweigh/sansaweighapp/SansaweighAppApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.sansaweigh.sansaweighapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SansaweighAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
